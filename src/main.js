4310a2553446af714e6098394a06d86ad4f003ae
Aidan Baack <aebaack@gmail.com>
2017-01-21 14:02:19 -0700

    Add index.html rendering on application load

--- src/main.js
+++ src/main.js
@@ -1,8 +1,21 @@
 'use strict';
 
 const electron = require('electron');
-const app = electron.app;
+const { app, BrowserWindow } = electron;
+
+let mainWindow; // Ensures garbage collection does not remove the window
 
 app.on('ready', () => {
-  console.log('Electron works!');
+  // Creates the application window with specified dimension
+  mainWindow = new BrowserWindow({
+    height: 750,
+    width: 750
+  });
+
+  // Loads index.html in as the main application page
+  mainWindow.loadURL(`file://${__dirname}/index.html`);
+
+  mainWindow.on('closed', () => {
+    mainWindow = null; // allow window to be garbage collected
+  });
 });
