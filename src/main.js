4e67607150345c22d84bfe73dcfafb128f949ac3
Aidan Baack <aebaack@gmail.com>
2017-01-21 14:12:02 -0700

    Update application dimensions to fill the screen

--- src/main.js
+++ src/main.js
@@ -6,10 +6,11 @@ const { app, BrowserWindow } = electron;
 let mainWindow; // Ensures garbage collection does not remove the window
 
 app.on('ready', () => {
-  // Creates the application window with specified dimension
+  // Creates the application window and sets its dimensions to fill the screen
+  const { width, height } = electron.screen.getPrimaryDisplay().workAreaSize;
   mainWindow = new BrowserWindow({
-    height: 750,
-    width: 750
+    width,
+    height
   });
 
   // Loads index.html in as the main application page
