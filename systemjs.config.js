b84e253b832dd8ff4cd68edc24f0f8260d6a4594
Aidan Baack <aebaack@gmail.com>
2017-01-23 14:01:07 -0700

    Add comments

--- systemjs.config.js
+++ systemjs.config.js
@@ -26,7 +26,7 @@
       // other libraries
       'rxjs':                      'npm:rxjs',
       'angular-in-memory-web-api': 'npm:angular-in-memory-web-api/bundles/in-memory-web-api.umd.js',
-      'fs': '@node/fs'
+      'fs': '@node/fs' // aliases 'fs' to the file system node module
     },
     // packages tells the System loader how to load when no filename and/or no extension
     packages: {
