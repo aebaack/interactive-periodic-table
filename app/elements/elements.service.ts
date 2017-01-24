eb86664672b6d2d42fb3332f8223f9790edfb5ac
Thomas Sarkisian <tsarkisian@gmail.com>
2017-01-23 18:21:48 -0700

    Remove unnecessary code

--- app/elements/elements.service.ts
+++ app/elements/elements.service.ts
@@ -8,9 +8,5 @@ export class ElementService {
   getElements(): any[] {
     const elements = readFileSync('./app/assets/elements.json', 'utf8');
     return JSON.parse(elements);
-    // return [{
-    //   atomicNumber: 1,
-    //   symbol: 'H'
-    // }]
   }
 }
