97f343e72ca8c05e2028bdc4599f8fb9776dfbbf
Aidan Baack <aebaack@gmail.com>
2017-01-24 15:49:25 -0700

    Add basic filter component

---
+++ app/filter/filter.component.ts
@@ -0,0 +1,12 @@
+import { Component} from '@angular/core';
+
+@Component({
+  moduleId: module.id,
+  selector: 'pt-filter',
+  templateUrl: 'filter.component.html',
+    styleUrls: ['filter.component.css'] 
+})
+
+export class FilterComponent {
+  
+}
