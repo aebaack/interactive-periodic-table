ff38f02bb23c4c022f2505eb5153eecc2a95d3b7
KatieAJenkins <kind2karma@gmail.com>
2017-01-24 11:24:15 -0700

    added bohrmodalcomponent

---
+++ app/bohr-modal/bohr-modal.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+import { ElementService } from '../elements/elements.service';
+
+@Component({
+  moduleId: module.id,
+  selector: 'bohr-modal',
+  templateUrl: 'bohr-modal.component.html',
+  styleUrls: ['bohr-modal.component.css']
+})
+
+export class BohrModalComponent {
+
+
+}
